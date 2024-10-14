f1d9fb49fdc3104ca7cbd564f4b261cf25c5a1da
dev123chauhan <76210158+dev123chauhan@users.noreply.github.com>
2024-10-15 00:15:16 +0530

    demo3

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { AuthProvider } from "./context/AuthContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter> 
     <AuthProvider>
       <App />
       </AuthProvider>
